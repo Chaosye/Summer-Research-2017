7d234730f2ec0f0952da67c7bfb608628ab875f9
Chaosye <nsmegan@gmail.com>
2017-06-19 15:25:45 -0700

    Added firebrowse functionality.

--- src/firehoseExp.py
+++ src/firehoseExp.py
@@ -14,11 +14,12 @@
 #####################################
 
 # Plan of Attack!  (\\ means complete)
-# Figure out how to handle nu Graphspace
-## Set up for three shapes (receptor, tf, none)
-## Set up for colored/individually directed connections (physical v phosphorylation)
-## Set up for multiple possible connections to the same node
-# Read in Wnt-edges.txt and Wnt-nodes.txt
+#Write a program to:
+##(1) get ONE TYPE of data from ONE TYPE of cancer from the API
+##(2) "map" the data to the NetPath nodes (NP nodes are in the UniProtID)
+##(3) get the data for every NODE (scaled/normalized to [0,1] (p-value?))
+##(4) annotate graph with this info.
+##(5) post to GS
 
 ##Start all functions with:
 #Input:
@@ -26,3 +27,18 @@
 #Output:
 #Initialize variables:
 #Variables initialized.
+
+#Pulls in firehose packages
+import sys
+sys.path.insert(0, '/afs/reed.edu/user/n/i/nicegan/Desktop/Summer-Research-2017/packages')
+import firebrowse
+from firebrowse import fbget
+
+def main():
+    print (fbget.mrnaseq(gene="wnt3", cohort="coad"))
+    return
+
+
+
+if __name__ == '__main__':
+    main()
